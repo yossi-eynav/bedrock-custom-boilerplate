b9802dfba356aceed104aaf35e929ffb306664bc
Yossi Eynav <yossi.eynav@gmail.com>
2015-12-30 21:29:14 +0200

    Added a page template sample.

--- web/app/mu-plugins/MainPlugin/Core/PluginManager.php
+++ web/app/mu-plugins/MainPlugin/Core/PluginManager.php
@@ -21,21 +21,30 @@ class PluginManager{
 
     public function addActions(){
         add_action('init',[$this,'initActionHandler']);
+
+        /**
+         * Make manipulation to the Admin Panel.
+         */
         add_action( 'login_enqueue_scripts', [$this,'styleAdminPanel'] );
         add_action('admin_head', [$this,'styleAdminPanel']);
         add_action('admin_menu', [$this,'addLogoToAdminMenu'], 100 );
     }
 
+    /**
+     * Add restart logo to the Admin Panel.
+     */
     public  function addLogoToAdminMenu(){
         add_menu_page( 'restart-group', '-', 'read', 'restart-group', function(){
-            echo '<iframe width="100%" height="1000" src="https://restartit.co.il" class="center-block"></iframe>';
+            echo '<iframe width="100%" height="1000" src="http://restartgroup.co" class="center-block"></iframe>';
         });
 
     }
+
+    /**
+     * Add custom stylesheet to the Admin Panel.
+     */
     public function styleAdminPanel(){
         wp_enqueue_style( 'custom-login', get_template_directory_uri() . '/dist/styles/main.css' );
-
-
     }
 
     public function addFilters(){
@@ -47,7 +56,7 @@ class PluginManager{
 
         // Change Wordpress login page logo href.
         add_filter('login_headerurl',function(){
-            return 'http://restartit.co.il';
+            return 'http://restartgroup.co';
         });
 
     }
@@ -61,10 +70,13 @@ class PluginManager{
 
     /** Add the ACF option page  */
     private function addACFOptionPages(){
-        acf_add_options_page("Footer");
+//        acf_add_options_page();
+
     }
 
-
+    /**
+     * Register Polylang strings.
+     */
     private function registerTranslatedStrings(){
         PolylangDao::getInstance()->initializeStrings();
     }
